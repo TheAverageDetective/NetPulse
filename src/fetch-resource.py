a002001deb7491a940779e1b8934e89af3892f88
Bhuvan M S <bhuvanms1605@gmail.com>
2026-03-21 15:41:11 +0530

    chore: project structure

---
+++ src/fetch-resource.py
@@ -0,0 +1,5 @@
+'''
+ the module is responsible for fetching the file from the host(cloud service)
+ records the timestamps for every download and computes the throughput
+ writes the result to a .csv file in `data/` 
+'''
