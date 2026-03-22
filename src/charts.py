1e568a625ba3bef9a9d5465d3ce3a2f044f64b0a
Anmol Vyas <70644006+TheAverageDetective@users.noreply.github.com>
2026-03-22 23:13:24 +0530

    Scheduling partially implemented

--- src/charts.py
+++ src/charts.py
@@ -1,9 +1,20 @@
-import pandas
+import pandas, numpy
 import matplotlib.pyplot as chart
 
 
-pandas.read_csv()
+data = pandas.read_csv("../data/test_set.csv", delimiter=",")
+total_rows = data["timestamp"].count()
 
-chart.savefig("temp.jpg")
+data = data[ data["result"] != "failed" ]       # We filter out failed downloads
+success_rows = data["timestamp"].count()
+
+
+# print(data)
+print("Failed rows: ", total_rows - success_rows)
+
+
+chart.plot(data["hour_of_day"], data["elapsed_transfer_s"], color="orange")
+
+chart.savefig("../data/chart.jpg")
 
 
