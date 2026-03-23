58b6bf87788489c5f5c6254102f9882e9e33bff4
Anmol Vyas <70644006+TheAverageDetective@users.noreply.github.com>
2026-03-23 22:49:20 +0530

    Chart and main code updated

--- src/dl_thread.py
+++ src/dl_thread.py
@@ -1,4 +1,11 @@
-import threading, time, sched, datetime
+"""
+This code will schedule the download to run at an interval.
+The scheduling and download happens on a separate thread from the main thread.
+"""
+
+import threading, time, sched
+from . import fetch_resource
+
 
 schedule = sched.scheduler(time.time, time.sleep)
 nextrun : sched.Event | None = None
@@ -7,22 +14,21 @@ scheduler_thread = threading.Thread(target=schedule.run, daemon=True)
 scheduler_thread_stopped = threading.Event()
 
 
-def run_download (interval: int, next_time : int) :
+def run_download (url : str, file : str, interval: int, next_time : int) :
     global nextrun
     print("Downloading at time", time.localtime().tm_hour)
+
     if not scheduler_thread_stopped.is_set():
         next_time += interval * 60
-        nextrun = schedule.enterabs(next_time, 1, run_download, [interval, next_time])
-        # TODO: call runner here
-        print("Running hahahah")
+        nextrun = schedule.enterabs(next_time, 1, run_download, [url, file, interval, next_time])
+        fetch_resource.runner(url, file)
 
 
-def start (interval : int) :
-    schedule.enterabs(time.time(), 1, run_download, [interval, time.time()])
+def start (url : str, interval : int, file: str) :
+    schedule.enterabs(time.time(), 1, run_download, [url, file, interval, time.time()])
     scheduler_thread.start()
 
 
 def stop () :
     scheduler_thread_stopped.set()
     if nextrun is not None : schedule.cancel(nextrun)
-
